8076d8f1c2d78fa787ea97f99db95a8191c061cd
GeunYeong <jgy091411@gmail.com>
2025-08-04 19:37:56 +0900

    App 테스트 - write 명령어 : TDD[레드&그린]

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class App {
+
+    private final Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        String input = sc.nextLine();
+        Rq rq = new Rq(input);
+
+        switch (rq.getMainCommand()) {
+            case "write" -> writeArticle();
+        }
+
+    }
+
+    private void writeArticle() {
+        System.out.println("게시글이 등록되었습니다.");
+    }
 }
