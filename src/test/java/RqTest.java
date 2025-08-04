cd6931c380947cc8b89debe2141f342443656308
GeunYeong <jgy091411@gmail.com>
2025-08-04 18:33:45 +0900

    Rq 테스트 - list 명령어 : TDD(블루)

--- src/test/java/RqTest.java
+++ src/test/java/RqTest.java
@@ -7,9 +7,22 @@ public class RqTest {
 
     @Test
     @DisplayName("write 명령어")
-    void test() {
+    void write() {
         //given
         String input = "write";
+
+        //when
+        Rq rq = new Rq(input);
+
+        //then
+        assertEquals(input, rq.getMainCommand());
+    }
+
+    @Test
+    @DisplayName("list 명령어")
+    void list() {
+        //given
+        String input = "list";
 
         //when
         Rq rq = new Rq(input);
