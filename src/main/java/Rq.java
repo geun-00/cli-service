9655b2ed469fc3c18ccbdadf7af9dc3064110ae1
GeunYeong <jgy091411@gmail.com>
2025-08-04 18:27:50 +0900

    Rq 테스트 - write 명령어 : TDD(그린)

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -2,11 +2,11 @@ import java.util.Map;
 
 public class Rq {
 
-    private String mainCommand;
+    private final String mainCommand;
     private Map<String, String> params;
 
     public Rq(String input) {
-
+        this.mainCommand = input;
     }
 
     public String getMainCommand() {
